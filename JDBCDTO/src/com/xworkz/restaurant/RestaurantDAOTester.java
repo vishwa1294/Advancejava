e3ee49afd7d6edb03bc5476fc1e3d37b62c0a6ed
Vaishnavijatti <83153420+vishwa1294@users.noreply.github.com>
2021-08-10 22:16:12 +0530

    Update RestaurantDAOTester.java

--- JDBCDTO/src/com/xworkz/restaurant/RestaurantDAOTester.java
+++ JDBCDTO/src/com/xworkz/restaurant/RestaurantDAOTester.java
@@ -8,11 +8,7 @@ import com.xworkz.restaurant.dto.RestaurantDTO;
 public class RestaurantDAOTester {
 	public static void main(String[] args) {
 		RestaurantDTO dto=new RestaurantDTO("Dwaraka Restaurant","Desai Circle","PavBhaji",true,RestaurantType.FAST_FOOD);
-		RestaurantDTO dto1=new RestaurantDTO("Ayodhya Restaurant","Ashok Circle","Dosa",false,RestaurantType.FAMILY);
-		
 		RestaurantDAO dao=new RestaurantDAOImpl();
 		dao.save(dto);
-		dao.save(dto1);
-	}
-
+		}
 }
