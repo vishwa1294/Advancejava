56052ea63d57053603f59937ae2352b29df70389
Vaishnavijatti <83153420+vishwa1294@users.noreply.github.com>
2021-08-11 22:23:00 +0530

    Update RestaurantDAO.java

--- JDBCDTO/src/com/xworkz/restaurant/dao/RestaurantDAO.java
+++ JDBCDTO/src/com/xworkz/restaurant/dao/RestaurantDAO.java
@@ -1,10 +1,22 @@
 
 package com.xworkz.restaurant.dao;
 
+import java.util.Collection;
+import com.xworkz.restaurant.constant.RestaurantType;
 import com.xworkz.restaurant.dto.RestaurantDTO;
 
 public interface RestaurantDAO {
 
-	int save(RestaurantDTO dto);
-
+int save(RestaurantDTO dto);
+	
+RestaurantDTO findByName(String name);
+	
+Collection<RestaurantDTO> findByType(RestaurantType type);
+	
+default boolean updateTypeByName(RestaurantType newtype,String name){
+return false;
+}
+default boolean deleteByTypeAndName(RestaurantType newtype,String name){
+return false;
+}
 }
