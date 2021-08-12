fe75ffe5aef8126064d19bf8f2ba0d4c858f4f2a
Vaishnavijatti <83153420+vishwa1294@users.noreply.github.com>
2021-08-12 09:27:09 +0530

    Update RestaurantDAOImpl.java

--- JDBCDTO/src/com/xworkz/restaurant/dao/RestaurantDAOImpl.java
+++ JDBCDTO/src/com/xworkz/restaurant/dao/RestaurantDAOImpl.java
@@ -35,5 +35,60 @@ public class RestaurantDAOImpl implements RestaurantDAO {
 		}
 		return 0;
 	}
+	@Override
+	public RestaurantDTO findByName(String name) {
+		try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+			String query = "select * from restaurant where r_name='" + name + "'";
+			Statement st = conn.createStatement();
+			st.execute(query);
+			ResultSet set = st.getResultSet();
+			if (set.next()) {
+				int id = set.getInt("r_id");
+				String rname = set.getString("r_name");
+				String rlocation = set.getString("r_location");
+				String rspcl = set.getString("r_specialFood");
+				boolean rbest = set.getBoolean("r_best");
+				String rtype = set.getString("r_type");
+
+				RestaurantDTO res = new RestaurantDTO(rname, rlocation, rspcl, rbest, RestaurantType.valueOf(rtype));
+				res.setId(id);
+				return res;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public Collection<RestaurantDTO> findByType(RestaurantType type) {
+		Collection coll = new ArrayList();
+		try (Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+			String str = "select * from restaurant where r_type='" + type + "'";
+			Statement st = con.createStatement();
+			st.execute(str);
+			ResultSet st = st.getResultSet();
+			if (st.next()) {
+				int resid = st.getInt("r_id");
+				String resname = st.getString("r_name");
+				String reslocation = st.getString("r_location");
+				String resspcl = st.getString("r_specialFood");
+				boolean resbest = st.getBoolean("r_best");
+				String restype = st.getString("r_type");
+
+				RestaurantDTO dto = new RestaurantDTO(resname, reslocation, resspcl, resbest,
+						RestaurantType.valueOf(restype));
+				dto.setId(resid);
+				coll.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return coll;
+	}
 
 }
+
+
